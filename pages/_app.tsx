7981ad2f86662d6b5896903018b340335b20fc8e
BANO.notIT <bano.notit@yandex.ru>
2023-02-07 19:35:30 +0300

    feat: Add dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,27 @@
-import "../styles/globals.css";
+import "~/shared/styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import {
+  darkTheme as rainbowDarkTheme,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
 import { type AppProps } from "next/app";
 import { WagmiConfig } from "wagmi";
 
+import { darkTheme as muiDarkTheme } from "~/shared/theme";
 import { chains, wagmiClient } from "~/shared/wagmi";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
-        <Component {...pageProps} />
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <ThemeProvider theme={muiDarkTheme}>
+      <CssBaseline />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains} theme={rainbowDarkTheme()}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </ThemeProvider>
   );
 }
 
---
+++ shared/theme/index.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+import { orange } from "@mui/material/colors";
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: orange[500],
+    },
+  },
+});
